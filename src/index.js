3c80d140ad249f750a813ff5ab37662eab792f24
Guille <guillempaniagua@gmail.com>
2026-02-04 18:59:58 +0000

    Inicio de la estructura y test de funcionamiento

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const user = {
+  name: 'Alice',
+  role: 'admin',
+  country: 'ES',
+};
+
+const message = `User ${user.name} has role ${user.role} and is located in ${user.country}`;
+
+console.log(message);
