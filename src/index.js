9564b722171d453dbb59344e0e6a0eb22028982f
Guille <guillempaniagua@gmail.com>
2026-02-05 18:38:57 +0000

    Creada la función principal que evalúa el acceso

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 const user = {
   name: 'Alice',
-  role: 'admin',
-  country: 'ES',
+  role: 'support',
 };
 
-const message = `User ${user.name} has role ${user.role} and is located in ${user.country}`;
+const action = 'delete';
 
-console.log(message);
+const resource = {
+  type: 'ticket',
+};
+
+// Función que evalúa si un usuario puede realizar una acción en un recurso
+const evaluateAccess = (user, action, resource) => {
+  if (user.role === 'admin') {
+    return {
+      allowed: true,
+      reason: 'Admin users can perform any action',
+    };
+  } else if (
+    user.role === 'support' &&
+    action === 'read' &&
+    resource.type === 'ticket'
+  ) {
+    return {
+      allowed: true,
+      reason: 'Support users can read tickets',
+    };
+  }
+
+  return {
+    allowed: false,
+    reason: 'Users does not have permission',
+  };
+};
+
+const decision = evaluateAccess(user, action, resource);
+
+console.log(
+  `Decision: ${decision.allowed ? 'ALLOW' : 'DENY'} - ${decision.reason}`
+);
